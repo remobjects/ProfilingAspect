9dc5c0f87468d90dce3a1e96f579512b3f067e20
Carlo Kok <ck@remobjects.com>
2015-08-27 13:49:38 +0200

    0: Add test project.

---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RemObjects.Profiler;
+
+namespace Test
+{
+	[__aspect: Profile]
+	static class Program
+	{
+		public static Int32 Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++)
+				Test();
+			return 0;
+		}
+		public static void Test() {
+			for (int i = 0; i < 15; i++)
+				InnerTest();
+		}
+
+		public static void InnerTest() 
+		{
+		System.Threading.Thread.Sleep(15);
+		}
+	}
+}
