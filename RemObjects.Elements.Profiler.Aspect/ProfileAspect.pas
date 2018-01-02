2661f702b34c625984a2cb8e5c77ca941c20ae7c
marc hoffman <mh@remobjects.com>
2018-01-02 09:31:48 -0400

    Allow an optional second conditional define to be provided for the [Profile] aspect

--- RemObjects.Elements.Profiler.Aspect/ProfileAspect.pas
+++ RemObjects.Elements.Profiler.Aspect/ProfileAspect.pas
@@ -1,7 +1,5 @@
 ﻿namespace RemObjects.Elements.Profiler;
 
-interface
-
 uses
   RemObjects.Elements.Cirrus.*;
 
@@ -9,40 +7,43 @@ type
   [AttributeUsage(AttributeTargets.Class or AttributeTargets.Method)]
   ProfileAspect = public class(IMethodImplementationDecorator)
   private
-    class var fShownWarning: Boolean;
+    var fOptionalDefine: String;
   public
+
+    constructor; empty;
+
+    constructor (aOptionalDefine: not nullable String);
+    begin
+      fOptionalDefine := aOptionalDefine;
+    end;
+
     method HandleImplementation(aServices: IServices; aMethod: IMethodDefinition);
-  end;
+    begin
+      if aMethod.Virtual = VirtualMode.Abstract then exit;
+      if aMethod.Empty then exit;
+      if aMethod.Owner.TypeKind = TypeDefKind.Interface then exit;
+      if aMethod.Name.StartsWith("get_") then exit;
+      if aMethod.Name.StartsWith("set_") then exit;
+      if aMethod.Name.StartsWith("add_") then exit;
+      if aMethod.Name.StartsWith("remove_") then exit;
 
-implementation
+      if not aServices.IsDefined('PROFILE') then
+        exit;
+      if (length(fOptionalDefine) > 0) and not aServices.IsDefined(fOptionalDefine) then
+        exit;
 
-method ProfileAspect.HandleImplementation(aServices: IServices; aMethod: IMethodDefinition);
-begin
-  if aMethod.Virtual = VirtualMode.Abstract then exit;
-  if aMethod.Empty then exit;
-  if aMethod.Owner.TypeKind = TypeDefKind.Interface then exit;
-  if aMethod.Name.StartsWith("get_") then exit;
-  if aMethod.Name.StartsWith("set_") then exit;
-  if aMethod.Name.StartsWith("add_") then exit;
-  if aMethod.Name.StartsWith("remove_") then exit;
-  if not aServices.IsDefined('PROFILE') then begin
-    if not fShownWarning then
-      aServices.EmitHint('Profiling aspect used but PROFILE not defined, aspect will be ignored');
-    fShownWarning := true;
-    exit;
+      var lType := aServices.GetType('RemObjects.Elements.Profiler.Profiler');
+      var lName := aMethod.Owner.Name+'.'+aMethod.Name+'(';
+      for i: Integer := 0 to aMethod.ParameterCount -1 do begin
+        if i <> 0 then lName := lName+',';
+        lName := lName+ aMethod.GetParameter(i).Type.Fullname;
+      end;
+      lName := lName+')';
+      aMethod.SurroundMethodBody(
+        new StandaloneStatement(new ProcValue(new TypeValue(lType), 'Enter', new DataValue(lName))),
+        new StandaloneStatement(new ProcValue(new TypeValue(lType), 'Exit', new DataValue(lName))),
+        SurroundMethod.Always);
+    end;
   end;
-
-  var lType := aServices.GetType('RemObjects.Elements.Profiler.Profiler');
-  var lName := aMethod.Owner.Name+'.'+aMethod.Name+'(';
-  for i: Integer := 0 to aMethod.ParameterCount -1 do begin
-    if i <> 0 then lName := lName+',';
-    lName := lName+ aMethod.GetParameter(i).Type.Fullname;
-  end;
-  lName := lName+')';
-  aMethod.SurroundMethodBody(
-    new StandaloneStatement(new ProcValue(new TypeValue(lType), 'Enter', new DataValue(lName))),
-    new StandaloneStatement(new ProcValue(new TypeValue(lType), 'Exit', new DataValue(lName))),
-    SurroundMethod.Always);
-end;
 
 end.
